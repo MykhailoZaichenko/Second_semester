99e7b4516a89a892b45aa4c2c4771097035a9690
MichailoZaichenko <michailo.zaichenko@gmail.com>
2025-03-01 22:44:42 +0200

    main functionality finished

--- Zaichenko_lab1_variant9/Zaichenko_lab1_variant9.cpp
+++ Zaichenko_lab1_variant9/Zaichenko_lab1_variant9.cpp
@@ -24,12 +24,12 @@ uint16_t pack(bool* input_arr, int n) {
 }
 
 // Функція для розпаковки 16-бітного числа в масив логічних значень
-void unpack(uint16_t packed, bool* output_arr, int& k, int* indices) {
+void unpack(uint16_t packed, bool* output_arr, int& k) {
     k = 0;
     for (int i = 0; i < 16; ++i) {
         output_arr[i] = (packed & (1 << i)) != 0;
         if (output_arr[i]) {
-            indices[k++] = i;
+            k++;
         }
     }
 }
@@ -38,8 +38,8 @@ void unpack(uint16_t packed, bool* output_arr, int& k, int* indices) {
 void print_binary(uint16_t number) {
     for (int i = 15; i >= 0; --i) {
         std::cout << ((number >> i) & 1);
-        if (i % 4 == 0) std::cout << ' ';
-        if (i % 8 == 0) std::cout << ' ';
+        if (i % 4 == 0 && i != 0) std::cout << ' ';
+        if (i % 8 == 0 && i != 0) std::cout << ' ';
     }
     //std::cout << std::endl;
 }
@@ -49,7 +49,7 @@ int main() {
 
     bool input_arr[ARRAY_SIZE] = { false };
     std::string input;
-    std::cout << "Input bit numbers, separated by commas: ";
+    std::cout << "Input bit position, separated by commas: ";
     std::getline(std::cin, input);
 
     std::stringstream ss(input);
@@ -72,22 +72,16 @@ int main() {
             std::cerr << "Error: Invalid input '" << value << "'." << std::endl;
             return 1;
         }
-        catch (const std::out_of_range& e) {
-            std::cerr << "Error: Input '" << value << "' is out of range." << std::endl;
-            return 1;
-        }
     }
 
     uint16_t packed = pack(input_arr, ARRAY_SIZE);
-    //std::cout << "Packed value: ";
+    std::cout << "Output: ";
     print_binary(packed);
-	std::cout << "; ";
+    std::cout << "; ";
     bool output_arr[ARRAY_SIZE] = { false };
     int k = 0;
-    int unpacked_indices[ARRAY_SIZE] = { 0 };
-    unpack(packed, output_arr, k, unpacked_indices);
+    unpack(packed, output_arr, k);
 
-    //std::cout << "Unpacked values: ";
     for (int i = 0; i < index; ++i) {
         if (i > 0) {
             std::cout << ",";
@@ -101,17 +95,28 @@ int main() {
 
 
 // testl
-// input: 1,3,2
-// output: 00011110; 1,3,2
+// Input bit position, separated by commas : 1, 3, 2
+// Output : 0000 0000  0000 1110; 1, 3, 2
+// 
 // test2
-// input: 3
-// output: 00000011; 3 
+// Input bit position, separated by commas : 3
+// Output : 0000 0000  0000 1000; 3
+// 
 // test3
-// input: 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
-// output: 11111111 11111111; 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+// Input bit position, separated by commas : 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+// Output : 1111 1111  1111 1111; 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
 // test4
-// input: -
-// output: 00000000 00000000; -
+// Input bit position, separated by commas :
+// Output: 0000 0000  0000 0000;
+// 
 // test5
-// input: 0, 15
-// output: 10000000 00000001; 0,15
+// Input bit position, separated by commas : 0, 15
+// Output : 1000 0000  0000 0001; 0, 15
+//
+// test6
+// Input bit position, separated by commas : 234
+// Error : Bit position 234 is out of range(0 - 15).
+//
+// test7
+// Input bit position, separated by commas : sdf
+// Error : Invalid input 'sdf'.
